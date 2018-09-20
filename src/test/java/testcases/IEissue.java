b3d84e7a00e7b0c442ac36ae2617e24654872f1e
Lengowski <Tomasz.Lengowski@dnvgl.com>
2018-09-20 11:52:07 +0200

    IEissues mod

--- src/test/java/testcases/IEissue.java
+++ src/test/java/testcases/IEissue.java
@@ -1,8 +1,10 @@
 package testcases;
 
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -27,8 +29,16 @@ public class IEissue {
 		WebDriver driver = new InternetExplorerDriver();
 		driver.manage().window().maximize();
 		driver.get("http://google.com");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	//	Thread.sleep(4000);
+		Thread.sleep(2000);
+		driver.manage().window().setSize(new Dimension(100,200));
+		Thread.sleep(2000);
+		driver.manage().window().setSize(new Dimension(200,100));
+		Thread.sleep(2000);
+		driver.manage().window().setSize(new Dimension(200,400));
+		Thread.sleep(2000);
+		driver.manage().window().setSize(new Dimension(200,200));
+		
+		Thread.sleep(2000);
 		try {
 			 Runtime.getRuntime().exec("taskkill /F /IM IEDriverServer.exe");
 			 Runtime.getRuntime().exec("taskkill /f /fi \"pid gt 0\" /im iexplore.exe");
